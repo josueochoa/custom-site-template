944d65ae9e0719b0ba99d238ee97ee2a5c04cd26
Josue Ochoa <m@josueochoa.com>
2019-08-02 10:29:42 -0500

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -9,51 +9,10 @@ WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
 DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
-# Make a database, if we don't already have one
-echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
-echo -e "\n DB operations done.\n\n"
-
 # Nginx Logs
 mkdir -p ${VVV_PATH_TO_SITE}/log
 touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
 touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
-
-if [ "${WP_TYPE}" != "none" ]; then
-
-  # Install and configure the latest stable version of WordPress
-  if [[ ! -f "${VVV_PATH_TO_SITE}/web/wp-load.php" ]]; then
-    echo "Downloading WordPress..." 
-    noroot wp core download --version="${WP_VERSION}"
-  fi
-
-  if [[ ! -f "${VVV_PATH_TO_SITE}/web/wp-config.php" ]]; then
-    echo "Configuring WordPress Stable..."
-    noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-define( 'WP_DEBUG', true );
-define( 'SCRIPT_DEBUG', true );
-PHP
-  fi
-
-  if ! $(noroot wp core is-installed); then
-    echo "Installing WordPress Stable..."
-
-    if [ "${WP_TYPE}" = "subdomain" ]; then
-      INSTALL_COMMAND="multisite-install --subdomains"
-    elif [ "${WP_TYPE}" = "subdirectory" ]; then
-      INSTALL_COMMAND="multisite-install"
-    else
-      INSTALL_COMMAND="install"
-    fi
-
-    noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
-  else
-    echo "Updating WordPress Stable..."
-    cd ${VVV_PATH_TO_SITE}/web
-    noroot wp core update --version="${WP_VERSION}"
-  fi
-fi
 
 cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 
